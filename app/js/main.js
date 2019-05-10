3b5ec1acfcabe6e16b00ee7a597c7333377adc64
nick-everhard <everhard@fex.net>
2019-05-10 13:27:56 +0300

    [nick] Вносит правки после первого теста

--- app/js/main.js
+++ app/js/main.js
@@ -72,3 +72,20 @@ $('.btn-review').click(function () {
 		alert('Заполните все поля!');
 	}
 });
+
+$(function() {
+  /* плавный скролл */
+  $(document).on('click', 'a[href^="#"]', function(evt) {
+    evt.preventDefault();
+    var $this = $(this);
+    var offset = 0;
+
+    if ($this.hasClass('head__link')) {
+      offset = 30;
+    }
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top - offset
+    }, 500);
+});
+});
